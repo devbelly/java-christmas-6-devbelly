3e2b43d039aa8cc73930d870bcb748af797b2e4b
Woongbae Yun <devbelly@naver.com>
2023-11-11 08:35:48 +0900

    feat : 메인은 티본스테이크, 바비큐립, 해산물파스타, 크리스마스파스타로 구성

--- src/main/java/christmas/domain/MenuDetail.java
+++ src/main/java/christmas/domain/MenuDetail.java
@@ -3,7 +3,11 @@ package christmas.domain;
 public enum MenuDetail {
     MUSHROOM_SOUP("양송이수프", new Money(6000)),
     TAPAS("타파스", new Money(5500)),
-    CAESAR_SALAD("시저샐러드", new Money(8000));
+    CAESAR_SALAD("시저샐러드", new Money(8000)),
+    T_BONE_STEAK("T본스테이크", new Money(55000)),
+    BARBECUE_RIBS("바비큐립", new Money(54000)),
+    SEAFOOD_PASTA("해산물파스타", new Money(35000)),
+    CHRISTMAS_PASTA("크리스마스파스타", new Money(25000));
 
     private String title;
     private Money price;
--- src/main/java/christmas/domain/MenuGroup.java
+++ src/main/java/christmas/domain/MenuGroup.java
@@ -4,9 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum MenuGroup {
-    APPETIZER("애피타이저",
-        Arrays.asList(MenuDetail.MUSHROOM_SOUP, MenuDetail.TAPAS, MenuDetail.CAESAR_SALAD)),
-    MAIN("메인", List.of()), DESSERT("디저트", List.of()), DRINK("음료", List.of());
+    APPETIZER("애피타이저", Arrays.asList(
+        MenuDetail.MUSHROOM_SOUP,
+        MenuDetail.TAPAS,
+        MenuDetail.CAESAR_SALAD)),
+    MAIN("메인", Arrays.asList(
+        MenuDetail.T_BONE_STEAK,
+        MenuDetail.BARBECUE_RIBS,
+        MenuDetail.SEAFOOD_PASTA,
+        MenuDetail.CHRISTMAS_PASTA)),
+    DESSERT("디저트", List.of()), DRINK("음료", List.of());
 
     private String title;
     private List<MenuDetail> menuDetails;

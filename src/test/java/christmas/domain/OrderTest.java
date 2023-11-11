d2349b0797b64e2ab18c85c19c5c755609c90b53
Woongbae Yun <devbelly@naver.com>
2023-11-11 15:03:55 +0900

    test : 총주문 금액 계산 기능 테스트 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -33,5 +34,20 @@ class OrderTest {
         assertThatThrownBy(() -> new Order(orderLines))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("[ERROR] 음료만 주문할 수 없습니다.");
+    }
+
+    @DisplayName("총 주문 금액은 각 메뉴의 구매 가격 합을 모두 더한 금액이다.")
+    @Test
+    void getTotalPrice() {
+        List<OrderLine> orderLines = List.of(
+            new OrderLine(MenuDetail.MUSHROOM_SOUP, 5),
+            new OrderLine(MenuDetail.T_BONE_STEAK, 6),
+            new OrderLine(MenuDetail.CHRISTMAS_PASTA, 7)
+        );
+
+        Order order = new Order(orderLines);
+
+        assertThat(order.getTotalAmounts()).isEqualTo(
+            new Money(6_000 * 5 + 55_000 * 6 + 25_000 * 7));
     }
 }

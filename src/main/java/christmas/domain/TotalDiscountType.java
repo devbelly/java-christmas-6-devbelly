456092bf88a6809690ec47f57d1f1a75ceae1802
Woongbae Yun <devbelly@naver.com>
2023-11-12 19:56:19 +0900

    feat : 특별 할인 생성 로직 추가

--- src/main/java/christmas/domain/TotalDiscountType.java
+++ src/main/java/christmas/domain/TotalDiscountType.java
@@ -1,13 +1,15 @@
 package christmas.domain;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public enum TotalDiscountType {
     CHRISTMAS_D_DAY(IntStream.range(1, 26).boxed().toList(),
         IntStream.range(1, 26).boxed().toList()),
-    SPECIAL(List.of(), IntStream.range(1, 31 + 1).boxed().toList()),
+    SPECIAL(List.of(3, 10, 17, 24, 25, 31), IntStream.range(1, 31 + 1).boxed().toList()),
     NONE(List.of(), List.of());
 
     private List<Integer> days;
@@ -20,5 +22,15 @@ public enum TotalDiscountType {
 
     public List<Integer> getDays() {
         return Collections.unmodifiableList(days);
+    }
+
+    public static TotalDiscountType findSpecialTypeByLocalDate(LocalDate localDate) {
+        int day = localDate.getDayOfMonth();
+
+        return Stream.of(TotalDiscountType.SPECIAL)
+            .filter(specialType -> specialType.periods.contains(day))
+            .filter(specialType -> specialType.days.contains(day))
+            .findAny()
+            .orElse(TotalDiscountType.NONE);
     }
 }

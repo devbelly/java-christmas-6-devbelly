a99b2d3feebd6a7d3b7b3493c2ce741c30b4bcef
Woongbae Yun <devbelly@naver.com>
2023-11-12 17:50:03 +0900

    feat : 평일 할인은 일요일부터 목요일까지 적용

--- src/main/java/christmas/domain/MenuDiscountType.java
+++ src/main/java/christmas/domain/MenuDiscountType.java
@@ -1,15 +1,22 @@
 package christmas.domain;
 
+import java.time.DayOfWeek;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public enum MenuDiscountType {
 
-    WEEKDAYS(IntStream.range(1, 31 + 1).boxed().toList()), WEEKENDS(List.of()), NONE(List.of());
+    WEEKDAYS(IntStream.range(1, 31 + 1).boxed().toList(),
+        List.of(DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
+            DayOfWeek.THURSDAY)),
+    WEEKENDS(List.of(), List.of()),
+    NONE(List.of(), List.of());
 
     List<Integer> days;
+    List<DayOfWeek> dayOfWeeks;
 
-    MenuDiscountType(List<Integer> days) {
+    MenuDiscountType(List<Integer> days, List<DayOfWeek> dayOfWeeks) {
         this.days = days;
+        this.dayOfWeeks = dayOfWeeks;
     }
 }

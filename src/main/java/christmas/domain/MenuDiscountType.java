a7345f52977da20789c1683c5e30aa1efdb56f7b
Woongbae Yun <devbelly@naver.com>
2023-11-12 16:50:52 +0900

    feat : 평일 할인은 1일부터 31일까지 적용

--- src/main/java/christmas/domain/MenuDiscountType.java
+++ src/main/java/christmas/domain/MenuDiscountType.java
@@ -1,5 +1,15 @@
 package christmas.domain;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public enum MenuDiscountType {
-    WEEKDAYS, WEEKENDS, NONE
+
+    WEEKDAYS(IntStream.range(1, 31 + 1).boxed().toList()), WEEKENDS(List.of()), NONE(List.of());
+
+    List<Integer> days;
+
+    MenuDiscountType(List<Integer> days) {
+        this.days = days;
+    }
 }

2c3bd2a1dec4ab74d5f0f7a9768591b23e96a552
Woongbae Yun <devbelly@naver.com>
2023-11-12 18:15:51 +0900

    feat : 특별 할인은 1일부터 31일까지 적용

--- src/main/java/christmas/domain/TotalDiscountType.java
+++ src/main/java/christmas/domain/TotalDiscountType.java
@@ -5,12 +5,17 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public enum TotalDiscountType {
-    CHRISTMAS_D_DAY(IntStream.range(1, 26).boxed().toList()), SPECIAL(List.of()), NONE(List.of());
+    CHRISTMAS_D_DAY(IntStream.range(1, 26).boxed().toList(),
+        IntStream.range(1, 26).boxed().toList()),
+    SPECIAL(List.of(), IntStream.range(1, 31 + 1).boxed().toList()),
+    NONE(List.of(), List.of());
 
     private List<Integer> days;
+    private List<Integer> periods;
 
-    TotalDiscountType(List<Integer> days) {
+    TotalDiscountType(List<Integer> days, List<Integer> periods) {
         this.days = days;
+        this.periods = periods;
     }
 
     public List<Integer> getDays() {

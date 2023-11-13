5cc26dcd110f3f247676bae32feb2bac4d31b4b9
Woongbae Yun <devbelly@naver.com>
2023-11-13 20:09:13 +0900

    feat : 혜택 미리보기 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,16 @@
 package christmas.view;
 
+import christmas.domain.Order;
+import christmas.domain.OrderLine;
+import java.time.LocalDate;
+
 public class OutputView {
 
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
+    private static final String BENEFIT_PREVIEW_MESSAGE = "%d월 %d일 에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
 
     private OutputView() {
     }
@@ -24,5 +29,11 @@ public class OutputView {
 
     public static void printOrderMenuMessage() {
         System.out.println(ORDER_MENU_MESSAGE);
+    }
+
+    public static void printBenefitPreviewMessage(LocalDate date) {
+        System.out.println(
+            BENEFIT_PREVIEW_MESSAGE.formatted(date.getMonthValue(), date.getDayOfMonth())
+        );
     }
 }

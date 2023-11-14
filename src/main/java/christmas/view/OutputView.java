7f8e0dba66721999b822ee2a602c462c5279bb73
Woongbae Yun <devbelly@naver.com>
2023-11-14 09:13:37 +0900

    fix : 불필요한 공백 제거

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -17,7 +17,7 @@ public class OutputView {
 
     private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    private static final String BENEFIT_PREVIEW_MESSAGE = "%d월 %d일 에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private static final String BENEFIT_PREVIEW_MESSAGE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
 
     private static final String ORDER_MENU = "<주문 메뉴>";
 

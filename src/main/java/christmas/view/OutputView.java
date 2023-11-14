a567981e60196818e10da8de324f2b585e8f380e
Woongbae Yun <devbelly@naver.com>
2023-11-14 09:10:06 +0900

    fix : 금액 출력 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,7 +12,9 @@ import java.util.List;
 public class OutputView {
 
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
     private static final String EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
     private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     private static final String BENEFIT_PREVIEW_MESSAGE = "%d월 %d일 에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
@@ -70,7 +72,7 @@ public class OutputView {
 
     public static void printTotalAmountsBeforeDiscount(Order order) {
         System.out.println(TOTAL_AMOUNTS_BEFORE_DISCOUNT);
-        System.out.println(order.getTotalAmounts());
+        printMoney(order.getTotalAmounts());
         printEmptyLine();
     }
 
